d577bebafc2519e1dd6b929a171b6a471ba10f6b
Dana Maimon <danamaimon@Mac.lan>
2025-12-05 01:47:03 +0200

    MyString

--- MyString.java
+++ MyString.java
@@ -17,14 +17,47 @@ public class MyString {
     }
 
     /** Returns the lowercase version of the given string. */
-    public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+    public static String lowerCase(String str) 
+    {
+        String s = "";
+        for (int i = 0; i < str.length(); i++)
+        {
+            char c = str.charAt(i);
+            if (c <= 90 && c >= 65) 
+            {
+                c = (char) (c + 32);
+            }
+            s = s + c;
+        }
+        return s;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
-    public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+    public static boolean contains(String str1, String str2) 
+    {
+        if (str2.length() > str1.length()) 
+        {
+            return false;
+        }
+        String s = "";
+        for (int i = 0; i < str2.length(); i ++)
+        {
+            for (int j = 0; j < str1.length(); j ++)
+            {
+                if (str2.charAt(i) == str1.charAt(j)) 
+                {
+                    s = s + str2.charAt(i);
+                    break;
+                }
+            }
+        }
+        if (s.equals(str2)) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
