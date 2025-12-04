612ddc255364beee63803e42e85b4e844d1657fd
Dana Maimon <danamaimon@Mac.lan>
2025-12-05 00:50:53 +0200

    Primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,34 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean[] primes = new boolean[n +1];
+        for (int i = 2; i < primes.length; i ++)
+        {
+            primes [i] = true;
+        }
+        int p = 2;
+        while (p <= Math.sqrt(n)) 
+        {
+            for (int i = 2; i < primes.length; i ++)
+            {
+                if (i != p && i % p == 0) 
+                {
+                   primes[i] = false; 
+                }
+            }
+            p++;
+        }
+        System.out.println("Prime numbers up to 30:");
+        int count = 0;
+        for (int i = 2; i < primes.length; i ++)
+        {
+            if (primes[i] == true) 
+            {
+                System.out.println(i);
+                count ++;
+            }
+        }
+        int precent = 100 * count / n;
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + precent + "% are primes)");
     }
 }
